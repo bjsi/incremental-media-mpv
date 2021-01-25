fc829d6b33a884088c4542c522d7319c43fc3a69
jamesb <experimentallearning0@gmail.com>
2021-01-25 09:39:33 +0000

    - Updated: run script to python

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+
+
+# TODO: pass in extra args
+def main(args):
+    subprocess.run([
+        "mpv",
+        "--script-opts=ytdl-format='bestvideo[height<=?1080][fps<=?30][vcodec!=?vp9]+bestaudio/best",
+        "--idle=once",
+        "--scripts=" + os.getcwd(),
+        "--ytdl-raw-options=no-check-certificate=",
+        ])
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
